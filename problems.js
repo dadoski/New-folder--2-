46f2960a0138c6fe4db39176ec90bfdd33a8cc62
dadoski <124809885+dadoski@users.noreply.github.com>
2023-04-28 21:57:36 +0400

    um

---
+++ problems.js
@@ -0,0 +1,38 @@
+/////// PROBLEM 1 ////////
+
+//my first name
+
+let firstName = "dado"
+console.log(firstName);
+
+//my last name
+let lastName = "gogilashvili"
+console.log(lastName);
+
+//my birthyear
+let birthYear = "2005"
+console.log(birthYear);
+
+//my gender
+let gender = "male"
+console.log(gender);
+
+
+
+//////// PROBLEM 2 ////////
+let answer = "4"
+console.log(answer);
+
+let equasionAnswer = "93"
+console.log(equasionAnswer); 
+
+
+
+
+//////// PROBLEM 3 ////////
+
+let country = "Brazil's population is"
+let population = "214.3 million"
+console.log(country+ ' ' +population);
+
+
